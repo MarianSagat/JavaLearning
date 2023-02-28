4551ce711b9e7e701bf34dac00e374564e04decc
z004rcrd <marian.sagat@gmail.com>
2023-02-28 15:23:00 +0100

    I.) more complicated Css selectors II.) basic-web page added - more complicated html+ css

--- src/test/java/CssSelectorTests.java
+++ src/test/java/CssSelectorTests.java
@@ -10,6 +10,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.stream.IntStream;
 
+//https://www.dofactory.com/css/ref/selectors
 //https://www.digitalocean.com/community/tutorials/css-css-selectors
 public class CssSelectorTests {
 
@@ -92,6 +93,114 @@ public class CssSelectorTests {
         testElements(elements,expectedResults);
 
         driver.quit();
+    }
+
+    /**
+     * <p>div * p</p>
+     **/
+    @Test
+    public void secondLevelDescendant_firstLevelArbitraryTag() {
+        WebDriver driver = new ChromeDriver();
+        driver.get(path_1.toUri().toString());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector("div * p"));
+        List<String> expectedResults = List.of("This is normal colored");
+        testElements(elements,expectedResults);
+
+        driver.quit();
+    }
+
+    final Path basicWebPath = Path.of("src/test/testResources/Basic-Web/index.html");
+
+
+    @Test
+    public void locateAllImagesFromSrcDir() {
+        WebDriver driver = new ChromeDriver();
+        driver.get(basicWebPath.toUri().toString());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector("img[src^=img]"));
+        Assert.assertEquals(elements.size(),9);
+
+        driver.quit();
+    }
+
+    @Test
+    public void locateOneImageOutsideOfDirSrc() {
+        WebDriver driver = new ChromeDriver();
+        driver.get(basicWebPath.toUri().toString());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+        //img which attribute src NOT start with img string
+        WebElement element = driver.findElement(By.cssSelector("img:not(img[src^=img])"));
+        String srcAttribute = element.getAttribute("src");
+        Assert.assertEquals(srcAttribute.substring(srcAttribute.lastIndexOf("/")+1).trim(), "logo6.jpg");
+
+        //only one not more not less
+        Assert.assertEquals(driver.findElements(By.cssSelector("img:not(img[src^=img])")).size(),1);
+        driver.quit();
+    }
+
+    @Test
+    public void twoDescendantClasses() {
+        WebDriver driver = new ChromeDriver();
+        driver.get(basicWebPath.toUri().toString());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+        //img which attribute src NOT start with img string
+        List<WebElement> elements = driver.findElements(By.cssSelector("div.col-12 p.lead"));
+        Assert.assertEquals(elements.size(),1);
+        WebElement foundedElement = elements.stream().findFirst().get();
+        Assert.assertTrue(foundedElement.getText().isEmpty());
+        //<p> containing "Welcome to my website tutorial!"
+        //for now i dont know how it checked without xpath, when i
+
+    }
+
+    @Test
+    public void severalDescendants() {
+        WebDriver driver = new ChromeDriver();
+        driver.get(basicWebPath.toUri().toString());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+        //img which attribute src NOT start with img string
+        List<WebElement> elements = driver.findElements(By.cssSelector("div div div div p"));
+        Assert.assertEquals(elements.size(),3);
+        WebElement foundedElement = elements.stream().findFirst().get();
+        Assert.assertTrue(foundedElement.getText().isEmpty());
+
+        //same but with simpler way
+        elements = driver.findElements(By.cssSelector("div p.card-text"));
+        Assert.assertEquals(elements.size(),3);
+        foundedElement = elements.stream().findFirst().get();
+        Assert.assertTrue(foundedElement.getText().isEmpty());
+        //TODO how i get the correct one element containging text inside <p></p> withou xpath?
+        driver.quit();
+    }
+
+    @Test
+    public void moreComplicatedConstruction()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get(basicWebPath.toUri().toString());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+        //img which attribute src NOT start with img string
+        List<WebElement> elements = driver.findElements(By.cssSelector("div.card-body p+a[class^=\"btn btn\"]"));
+        Assert.assertEquals(elements.size(), 3);
+        WebElement foundedElement = elements.stream().findFirst().get();
+        Assert.assertTrue(foundedElement.getText().isEmpty());
+
+
+        //UNIQUE SELECTION OF ONLY ONE FROM THESE 3
+        List<WebElement> elements_2 = driver.findElements(By.cssSelector("div.card img[src*=\"team1\"]+div >a"));
+        Assert.assertEquals(elements_2.size(), 1);
+        WebElement foundedElement_2 = elements.stream().findFirst().get();
+        Assert.assertTrue(foundedElement_2.getText().isEmpty());
+
+        driver.quit();
+        //TODO i would find if foundedElement_2 is in elements
     }
 
     /* TODO use testNg template tests, because these 3 tests are the same, only difference is cssSelector and expectedResults, same code */
