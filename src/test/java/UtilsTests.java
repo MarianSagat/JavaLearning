2826d3273642d317b972626167abb11dc63da931
z004rcrd <marian.sagat@gmail.com>
2023-10-11 14:29:32 +0200

    utils tests

---
+++ src/test/java/UtilsTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class UtilsTests {
+    @Test
+    public void round() {
+        System.out.println(Math.round(2.4));
+        System.out.println(Math.round(2.8));
+    }
+}
